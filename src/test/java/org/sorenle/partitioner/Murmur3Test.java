77f494097ebcb3e20e640d5d055c929cd866c5bd
Salih Gedik <dev@salihgedik.com>
2017-08-12 02:40:59 +0300

    Murmur3 partitioner

--- src/test/java/org/sorenle/partitioner/Murmur3Test.java
+++ src/test/java/org/sorenle/partitioner/Murmur3Test.java
@@ -1,9 +1,21 @@
 package org.sorenle.partitioner;
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-public class Murmur3Test {
 
+import java.nio.charset.Charset;
+
+public class Murmur3Test {
+    static Logger logger = LogManager.getLogger(MurmurHash3.class);
 
     @Test
     public void generateMurmurHash(){
+        HashFunction hf = Hashing.murmur3_128();
+        HashCode hc = hf.newHasher().putString("Hey there", Charsets.UTF_8).hash();
+        logger.info(hc.toString());
     }
 }
