689c063e1fbc4c0f692db435dd3e84f4256e4459
Salih Gedik <dev@salihgedik.com>
2017-08-16 01:08:27 +0300

    Cluster class representation

--- src/test/java/org/sorenle/partitioner/node/NodeStatusTest.java
+++ src/test/java/org/sorenle/partitioner/node/NodeStatusTest.java
@@ -25,4 +25,15 @@ public class NodeStatusTest {
         NodeStatus nodeStatus = new NodeStatus(NodeStatusCode.UP);
         assertTrue(nodeStatus.isReadyToJoin());
     }
+
+    @Test
+    public void joinAndFail(){
+        NodeStatus nodeStatus = new NodeStatus(NodeStatusCode.UP);
+        assertTrue(nodeStatus.isReadyToJoin());//UP but not joined
+        nodeStatus.setJoinStatus(NodeStatusCode.UP_AND_JOINED);
+        assertTrue(nodeStatus.hasJoined()); // Joined
+        nodeStatus.setJoinStatus(NodeStatusCode.DOWN);// Down
+        assertTrue(nodeStatus.isDown());
+
+    }
 }
