3d046aa469bed6e9d42ed7ccc12aaf5150880e33
Salih Gedik <dev@salihgedik.com>
2017-08-16 00:50:50 +0300

    NodeStatus implementation

--- src/main/java/org/sorenle/node/NodeStatus.java
+++ src/main/java/org/sorenle/node/NodeStatus.java
@@ -1,5 +1,35 @@
 package org.sorenle.node;
 
+import javax.xml.soap.Node;
+
 public class NodeStatus {
     //TODO implement
+    private NodeStatusCode joinStatus;
+    private boolean onSync;
+
+    public NodeStatusCode getJoinStatus() {
+        return joinStatus;
+    }
+
+    public void setJoinStatus(NodeStatusCode joinStatus) {
+        this.joinStatus = joinStatus;
+    }
+
+    public boolean onSync() {
+        return onSync;
+    }
+
+    public void setOnSync(boolean onSync) {
+        this.onSync = onSync;
+    }
+
+    public boolean hasJoined(){
+        return joinStatus.equals(NodeStatusCode.UP_AND_JOINED);
+    }
+    public boolean isDown(){
+        return joinStatus.equals(NodeStatusCode.DOWN);
+    }
+    public boolean isReadyToJoin(){
+        return joinStatus.equals(NodeStatusCode.UP);
+    }
 }
