8c626acae14ef0ec3bec6a1c2b13ac1de1d771f1
Salih Gedik <dev@salihgedik.com>
2017-08-16 02:34:35 +0300

    Test class for Murmur3Builder

--- src/test/java/org/sorenle/partitioner/Murmur3BuilderTest.java
+++ src/test/java/org/sorenle/partitioner/Murmur3BuilderTest.java
@@ -1,4 +1,28 @@
 package org.sorenle.partitioner;
 
+import com.google.common.hash.Hashing;
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class Murmur3BuilderTest {
+
+
+    @Test
+    public void test128BitPartitioner(){
+        Murmur3Partitioner murmur3Partitioner = Murmur3Builder.create128BitHasher();
+        long hashExpected = Hashing.murmur3_128().newHasher().putInt(0).hash().padToLong();
+        long hash = murmur3Partitioner.getHash(0);
+
+        assertEquals(hash,hashExpected);
+
+    }
+
+    @Test
+    public void test32BitPartitioner(){
+        Murmur3Partitioner murmur3Partitioner = Murmur3Builder.create32BitHasher();
+        long hashExpected = Hashing.murmur3_32().newHasher().putInt(0).hash().padToLong();
+        long hash = murmur3Partitioner.getHash(0);
+
+        assertEquals(hash,hashExpected);
+
+    }
 }
