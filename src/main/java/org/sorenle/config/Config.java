23a3f45d3a1e3debca297d611fccc5902f85290b
Salih Gedik <dev@salihgedik.com>
2017-08-23 17:31:55 +0300

    Config object to hold values those are parsed from file

--- src/main/java/org/sorenle/config/Config.java
+++ src/main/java/org/sorenle/config/Config.java
@@ -1,4 +1,15 @@
 package org.sorenle.config;
 
 public class Config {
+
+    public String cluster_name;
+
+    // How frequently should gossip check for alive nodes in cluster in milliseconds
+    public Long gossip_status_check_interval;
+
+    //Indicates number of maximum reconect
+    public Integer maximum_reconnect_tomark_dead;
+
+
+
 }
